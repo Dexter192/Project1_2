733e6c6f31798814a04878ae65ec7da3fdf7fa8a
DanielKaestner@outlook.de <DanielKaestner@outlook.de>
2018-03-18 14:57:57 +0100

    Added the required Libgdx libaries to the project.

---
+++ src/Golf.java
@@ -0,0 +1,23 @@
+import org.lwjgl.opengl.GL30;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+public class Golf extends Game{
+
+	/**
+	 * Implement the game initialization here. That should be stuff like the 
+	 * course builder, the menu screen or and maybe the physics engine 
+	 */
+	@Override
+	public void create() {
+	}
+
+	/**
+	 * This method updates the game. So every drawing, collision detection 
+	 * etc. should be called here
+	 */
+	@Override
+	public void render() {
+	}
+}
