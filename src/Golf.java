daed57f67da56ebd57377e89e5483daef3af16d4
thaleaschlender <thalea.schlender@gmail.com>
2018-03-19 12:16:20 +0100

    sdf

--- src/Golf.java
+++ src/Golf.java
@@ -1,14 +1,9 @@
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Rectangle;
 
 
 public class Golf extends Game{
