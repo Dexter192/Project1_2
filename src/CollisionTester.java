5b73332a3f9891fcd6b19387bf80233834429285
Olive <Olive@Converty.wireless.unimaas.local>
2018-03-21 12:08:17 +0100

    Collision detection code from scratch. Seems to work but please can someone test it thoroughly.

---
+++ src/CollisionTester.java
@@ -0,0 +1,18 @@
+public class CollisionTester 
+{
+	public static void main(String[] args)
+	{
+		BoundingSphere bir = new BoundingSphere(5, 0, 0, 0);
+		//BoundingSphere iki = new BoundingSphere(5, 0, 0, 0);
+		//AABB uc = new AABB(0, 0, 0, 3, 3, 3);
+		AABB dort = new AABB(5, 5, 5, 15, 15, 15);
+		//boolean alive = bir.intersectingBS(iki);
+		//boolean another = uc.intersectingAABB(dort);
+		boolean andAgain = dort.intersectingSB(bir);
+		//System.out.println("Bir and iki' intersection's existance is " + alive);
+		//System.out.println("Uc and dort's intersection's existance is " + another);
+		System.out.println("Bir and dort's intersection's existance is " + andAgain);
+		andAgain = bir.intersectingAABB(dort);
+		System.out.println("Bir and dort's intersection's existance is " + andAgain);
+	}
+}
